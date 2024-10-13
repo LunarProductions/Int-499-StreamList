f3bf40d04f99c1875e1b99ac547dd910c555b36b
LunarProductions <50446569+LunarProductions@users.noreply.github.com>
2024-10-13 03:16:31 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import StreamList from './Components/StreamList';
+import Movies from './Components/Movies';
+import Cart from './Components/Cart';
+import About from './Components/About';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
